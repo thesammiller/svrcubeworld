9fb795270a225c2aec0f2fc0255c2f1d1eeaf448
Samuel Miller <samuelsmiller@gmail.com>
2021-09-26 08:10:34 -0400

    Reorganize.

--- tests/ovr_test.cc
+++ tests/ovr_test.cc
@@ -1,10 +1,11 @@
 #include <gtest/gtest.h>
 
-#include "OVR_Bridge.h"
+#include "../OVR_Bridge.h"
 
-TEST(HelloTest, BasicAssertions) {
+TEST(RandomFloatTest, BasicAssertions) {
   float random_a = RandomFloat();
   float random_b = RandomFloat();
 
   ASSERT_FALSE(random_a == random_b);
 }
+
