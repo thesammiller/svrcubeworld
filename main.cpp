f1e9339787f44547673d2b43ad44b7c44fea6ab2
Samuel Miller <samuelsmiller@gmail.com>
2021-10-02 12:16:27 -0400

    Refactored update. Cleaner server implementation.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 
 
-
-
 #include "svrAppl.h"
 
 //OVR
@@ -12,13 +10,9 @@
 #include <iostream>
 #include <vector>
 #include <sys/resource.h>
-//TAO
-#include "test_i.h"
-#include "tao/IORTable/IORTable.h"
-#include "ace/Get_Opt.h"
-#include "ace/OS_NS_stdio.h"
-#include "ace/Task.h"
-#include "ServerWorker.h"
+
+#include "svrServer.h"
+
 
 
 //TAO
@@ -26,6 +20,7 @@ int niterations = 10;
 int nthreads = 1;
 
 // GL Functions -- Resize Window Callback, ProcessInput
+//TODO I think this first one can be removed
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(svrAppl appl);
 
@@ -35,104 +30,37 @@ const unsigned int SCR_HEIGHT = 600;
 
 // TAO 
 //----------------------------------------------
-CORBA::ORB_var orb;
-const ACE_TCHAR *ior_output_file = ACE_TEXT("test.ior");
 
-int
-parse_args (int argc, ACE_TCHAR *argv[])
-{
-  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("o:"));
-  int c;
 
-  while ((c = get_opts ()) != -1)
-    switch (c)
-      {
-      case 'o':
-        ior_output_file = get_opts.opt_arg ();
-        break;
-
-      case '?':
-      default:
-        ACE_ERROR_RETURN ((LM_ERROR,
-                           "usage:  %s "
-                           "-o <iorfile>"
-                           "\n",
-                           argv [0]),
-                          -1);
-      }
-  // Indicates successful parsing of the command line
-  return 0;
-}
 
 int main(int argc, char* argv[])
 {
+      //Server myServer;
+      //Initialize orb object
+      //myServer.createOrb();
+      //Spin off a worker
+      //myServer.configServer();
+      //myServer.createWorker()
    
-       orb =
-        CORBA::ORB_init (argc, argv);
-
-      CORBA::Object_var poa_object =
-        orb->resolve_initial_references("RootPOA");
-
-      PortableServer::POA_var root_poa =
-        PortableServer::POA::_narrow (poa_object.in ());
-      if (CORBA::is_nil (root_poa.in ()))
-        ACE_ERROR_RETURN ((LM_ERROR,
-                           " (%P|%t) Unable to initialize the POA.\n"),
-                          1);
-
-      PortableServer::POAManager_var poa_manager =
-        root_poa->the_POAManager ();
-
-      if (parse_args (argc, argv) != 0)
-        return 1;
-
-      Simple_Server_i server_impl (orb.in ());
-
-      PortableServer::ObjectId_var id =
-        root_poa->activate_object (&server_impl);
-
-      CORBA::Object_var object = root_poa->id_to_reference (id.in ());
-
-      Simple_Server_var server =
-        Simple_Server::_narrow (object.in ());
-
-      CORBA::String_var ior =
-        orb->object_to_string (server.in ());
-
-      CORBA::Object_var table_object =
-        orb->resolve_initial_references("IORTable");
-
-      IORTable::Table_var table =
-        IORTable::Table::_narrow (table_object.in ());
-      if (CORBA::is_nil (table.in ()))
-        ACE_ERROR_RETURN ((LM_ERROR,
-                           " (%P|%t) Unable to initialize the IORTable.\n"),
-                          1);
-      table->bind ("Simple_Server", ior.in ());
-
-      ACE_DEBUG ((LM_DEBUG, "Activated as <%C>\n", ior.in ()));
-
-      FILE *output_file= ACE_OS::fopen (ior_output_file, "w");
-      if (output_file == 0)
-        ACE_ERROR_RETURN ((LM_ERROR,
-                           "Cannot open output file for writing IOR: %s",
-                           ior_output_file),
-                          1);
-      ACE_OS::fprintf (output_file, "%s", ior.in ());
-      ACE_OS::fclose (output_file);
-
-      poa_manager->activate ();
-      Worker worker(orb.in());
-      worker.activate(THR_NEW_LWP | THR_JOINABLE, nthreads);
+    svrServer myServer = svrServer();
+    myServer.createOrb(argc, argv);
+    myServer.createServer();
      
     
     //Initialize the application
     svrAppl myAppl = svrAppl();
-    
+    // Create main window
     myAppl.createWindow(SCR_WIDTH, SCR_HEIGHT, "CubeWorld");
+    //Create shader program for GPU
+    //TODO: Pass in shader file parameters
     myAppl.createShader();
+    // Create the CubeWorld vertex data
     myAppl.createWorld();
+    // Create the GLM Matrices for Cameras, set camera position
     myAppl.createCamera();
+
+    Worker worker(myServer.orb.in());
+    worker.activate(THR_NEW_LWP | THR_JOINABLE, nthreads);
     
     // render loop
     // -----------
@@ -140,15 +68,20 @@ int main(int argc, char* argv[])
     {
         // input
         // -----
+        //This just checks for the escape key (which should trigger window should close)
+        //TODO: This just kills the world... the hanging thread below needs to be fixed.
         processInput(myAppl);
 
+        //myServer.getData();
+
         // Take the server data and update the view based on controller input
-        myAppl.updateView(server_impl.data[0], server_impl.data[1]);
+        myAppl.updateView(myServer.server_impl.data[0], myServer.server_impl.data[1]);
         
         // Draw
         myAppl.render();
 
         //TODO: Send drawing to client
+        //myServer.sendDrawing();
 
     }
 
