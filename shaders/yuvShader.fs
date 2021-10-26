41e3e0963bc0bda62e37c2da4f2fb766a5a7d5bd
Samuel Miller <samuelsmiller@gmail.com>
2021-10-26 08:46:21 -0400

    Closeish

--- shaders/yuvShader.fs
+++ shaders/yuvShader.fs
@@ -15,17 +15,17 @@ uniform sampler2D textureV;
 
 //https://www.libsdl.org/tmp/SDL/src/render/opengl/SDL_shaders_gl.c
 // YUV offset
-//const vec3 offset = vec3(-0.0627451017, -0.501960814, -0.501960814);
-const vec3 offset = vec3(-0.0625, -0.5, -0.5);
+const vec3 offset = vec3(-0.0627451017, -0.501960814, -0.501960814);
+//const vec3 offset = vec3(-0.0625, -0.5, -0.5);
 
 // RGB coefficients \n"                           
-const vec3 Rcoeff = vec3(1.1644,  0.000,  1.7927);
-const vec3 Gcoeff = vec3(1.1644, -0.2132, -0.5329);
-const vec3 Bcoeff = vec3(1.1644,  2.1124,  0.000);
+//const vec3 Rcoeff = vec3(1.1644,  0.000,  1.7927);
+//const vec3 Gcoeff = vec3(1.1644, -0.2132, -0.5329);
+//const vec3 Bcoeff = vec3(1.1644,  2.1124,  0.000);
 
-//const vec3 Rcoeff = vec3(1.1644,  0.000,  1.596);           \
-//const vec3 Gcoeff = vec3(1.1644, -0.3918, -0.813);          \
-//const vec3 Bcoeff = vec3(1.1644,  2.0172,  0.000);
+const vec3 Rcoeff = vec3(1.1644,  0.000,  1.596);           \
+const vec3 Gcoeff = vec3(1.1644, -0.3918, -0.813);          \
+const vec3 Bcoeff = vec3(1.1644,  2.0172,  0.000);
 
 
 void main()
